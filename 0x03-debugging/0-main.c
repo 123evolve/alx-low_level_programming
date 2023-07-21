0f7c7fa5b72ae4a23fb96d3441bf37792cea106d
Jesutofunmi <jesutofunmijesuseyi@gmail.com>
2023-07-21 16:47:36 -0700

    Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,32 +1,16 @@
 #include "main.h"
 
 /**
-<<<<<<< HEAD
 * main - tests function that prints if integer is positive or negative
 * Return: 0
 */
 
 int main(void)
 {
-        int i;
+				int i;
 
-        i = 0
-=======
- * main - Test function for position or negative
- * Return: 0
- */
+				i = 98;
+				positive_or_negative(i);
 
-int main(void)
-{	
- 	int i;
-
-	i = 0;
->>>>>>> 281fc78f8798aa2d7e5dd2ae619fb6b8600b30b6
-	positive_or_negative(i);
-
-return (0);
+		return (0);
 }
-<<<<<<< HEAD
-
-=======
->>>>>>> 281fc78f8798aa2d7e5dd2ae619fb6b8600b30b6
