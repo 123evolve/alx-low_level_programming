21387c94661b2499e5709b6ced56cf6cd201dc0f
Jesutofunmi <jesutofunmijesuseyi@gmail.com>
2023-07-24 15:52:27 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,6 +8,7 @@
 void print_rev(char *s)
 {
 	int longi = 0;
+
 	int 0;
 
 	while (*s != '\0')
@@ -16,7 +17,7 @@ void print_rev(char *s)
 		s++;
 	}
 	s--;
-	for (0=longi; 0 >0; = 0--)
+	for (0 = longi; 0 > 0; = 0--)
 	{
 		_putschar(*s);
 		s--;
