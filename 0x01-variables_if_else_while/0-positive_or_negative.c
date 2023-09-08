5484372e342301591926533bfc7bd6beb584671b
Pamilerin Jesutofunmi <135073154+123evolve@users.noreply.github.com>
2023-09-08 23:14:33 +0100

    Update 0-positive_or_negative.c
    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,7 @@
 
 /**
  * main - Entry point
- * 
+ * Description: 'Check for numbers if it is positive or negative'$
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -15,11 +15,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("%d is positive\n", n);
+	printf("%d is positive\n", n);
 	}
 	else if (n == 0)
 	{
-		printf("%d is zero\n", n);
+	printf("%d is zero\n", n);
 	}
 	else
 	{
