579976a28dd56068eed0cfa31641dd0c4157fe24
Jesutofunmi Pamilerin <jesutofunmijesuseyi@gmail.com>
2023-09-12 05:03:10 +0100

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,10 +7,18 @@
 
 int main(void)
 {
-	int a;
+	int n = 97;
 
-	for (a = 0; a < 10; a++)
-		printf("%d", a);
+	while (n <= 122)
+	{
+		if (n == 101 || n== 113)
+		{
+			n++;
+			continue;
+		}
+		putchar(n);
+		n++
+	}
 	putchar('\n');
 	return (0);
 }
