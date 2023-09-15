1f494795b0ca9c6a11eacaad1030b4a7bba69e3c
Jesutofunmi Pamilerin <jesutofunmijesuseyi@gmail.com>
2023-09-15 08:25:44 +0100

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Make the alphabet
+ *
+ * Return: void
+ */
+
+void print_alphabeth(void)
+{
+ char ch;
+ for (ch = 'a'; ch <= 'z'; ch++)
+ _putchar(ch);
+ _putchar('\n');
+ }
